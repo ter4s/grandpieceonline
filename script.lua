ce1d142f7ae2b647b0e5556078c12c70179662d3
teres <76972302+ter4s@users.noreply.github.com>
2023-07-12 10:17:18 -0300

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,14 @@
+
+local placeId = game.PlaceId
+
+if placeId == 6152116144 or placeId == 13883279773 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ter4s/grandpieceonline/main/Protected_1373076712025792.lua"))()
+elseif placeId == 11468159863 or placeId == 13881804983 or placeId == 13883059853 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ter4s/grandpieceonline/main/Protected_2123068629006087.lua"))()
+elseif placeId == 5956785391 then 
+    -- menu is not complete
+elseif placeId == 11468034852 then -- MUGEN
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ter4s/grandpieceonline/main/Protected_7178450311501662.lua"))()
+else --ouwi
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ter4s/grandpieceonline/main/Protected_7178450311501662.lua"))()
+end
